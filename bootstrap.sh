d6e1e202f476858b64a8995d6ecd765a71ce333c
Laurent De Marez <laurent@demarez.org>
2013-09-03 21:41:19 +0200

    Xdefaults + bootstrap: Add Xdefaults and update bootsrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,7 @@ ensure_link "zsh/zshrc"     ".zshrc"
 ensure_link "irssi"         ".irssi"
 ensure_link "xbindkeysrc"   ".xbindkeysrc"
 ensure_link "gitconfig"     ".gitconfig"
+ensure_link "Xdefaults"     ".Xdefaults"
 
 mkdir -p "$DIR/vim/tmp/undo"
 mkdir -p "$DIR/vim/tmp/backup"
