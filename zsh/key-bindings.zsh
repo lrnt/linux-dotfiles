c6ccd2bc163e3e5023fde915154b806f1440af82
Laurent De Marez <laurent.demarez@bmw-carit.de>
2013-10-22 16:46:07 +0200

    zsh/key-bindings: Add new key bindings

---
+++ zsh/key-bindings.zsh
@@ -0,0 +1,14 @@
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+
+bindkey "^[[H" beginning-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[OH" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[4~" end-of-line
+bindkey "^[OF" end-of-line
+
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+
+bindkey '^r' history-incremental-search-backward
