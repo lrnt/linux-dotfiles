041483fbf4b9820c5ccc559952f16e0c0575f8ef
Laurent De Marez <laurent@demarez.org>
2017-08-17 12:40:27 +0200

    Add a rofi replacement for passmenu

---
+++ bin/rofi-pass.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob globstar
+
+# Handle argument.
+if [ ! -z "$@" ]
+then
+    coproc (pass -c "$@")
+    exit;
+fi
+
+# Print password list
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+printf '%s\n' "${password_files[@]}"
